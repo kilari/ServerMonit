27c61d8d934e08f54718c2ef57db6d84656f8b28
kilari vamsi krishna <vamsikilari@gmail.com>
2009-05-06 13:42:47 -0400

    Added the down ping class to check the down hosts separately

--- ping1.rb
+++ ping1.rb
@@ -19,24 +19,67 @@ end
 class PingClass
         def httpcheck(host = "74.63.10.108")
 	if Net::PingTCP.new(host, 80).ping
-	puts "#{host} is alive"
+	puts "alive #{host} from pingclass"
+#	puts "#{host} is alive"
 
 	else 
-	puts "I belive #{host} dead, sending alert"
-	SendAlert::SendMail.downalert(host)
+#	puts "I belive #{host} dead, sending alert"
+	puts "Down #{host} from pingclass"
+	$downips << host
+	$ipstore.delete_at($ipstore.index(host))
 
+#	SendAlert::SendMail.downalert(host)
+
+	end
+	end
+end
+
+class DownPingClass
+	
+	$downips = []
+	def downhttpcheck(host)
+        unless $downips.empty?
+	if Net::PingTCP.new(host, 80).ping
+	$ipstore << host
+	$downips.delete_at($downips.index(host))
+	puts "up #{host} from downpingclass"
+#	SendAlert::SendMail.upalert(host)
+	else 
+	puts "down #{host} from downpingclass"
+#	SendAlert::SendMail.downalert(host)
+	end
 	end
 	end
 end
 
 class DataInitClass
         
-	def init()     
+	def StoreToPool
 	fcd = FetchDataClass.new()
-	fcd.fetch
+	fcd.fetch	
+	end
+
+	def init()     
+#	fcd = FetchDataClass.new()
+#	fcd.fetch
+	unless $downips.empty? 
+#	if !$downips.empty?
+	$downips.each do |downip|
+	DownPingClass.new.downhttpcheck(downip) #unless $downips.empty?
+	end
+	end 
 	$ipstore.each do |ipsend|
 	PingClass.new.httpcheck(ipsend)
 	end
+	end	
+end
+
+class ShowPool
+	def downpool
+	$downips.inspect
+	end
+	def uppool
+	$ipstore.inspect
 	end
 end
 end
